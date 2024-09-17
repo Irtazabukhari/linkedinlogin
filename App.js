23f6f2952dd1e229a1206ed1238fa7dadf438311
Irtazabukhari <syedirtaza193@gmail.com>
2024-09-17 19:04:54 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View , StatusBar, ScrollView} from 'react-native';
+import MyPostCard from './components/mycard';
+
+export default function App() {
+  return (
+    <View>
+      <MyPostCard />
+    </View>
+  );
+};
+
+
